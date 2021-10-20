6856e0777b0d9cf47a0ff2e9214e9fa4af972ddf
srymaker0 <zyjay0@gmail.com>
2021-10-20 23:39:45 +0800

    feat(global): 增加颜色调试信息宏定义, 增加配置文件获取功能

--- common/common.h
+++ common/common.h
@@ -7,8 +7,15 @@
 
 #ifndef _COMMON_H
 #define _COMMON_H
+
+#ifdef _D
+#define DBG(fmt, args...) printf(fmt, ##args)
+#else
+#define DBG(fmt, args...)   
+#endif
 int make_nonblock(int fd);
 int make_block(int fd);
 int socket_create(int port);
 int socket_connect(const char *ip, int port);
+char *get_conf_value(const char *filename, const char *key);
 #endif
