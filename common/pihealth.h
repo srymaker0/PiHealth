0821de65aece5a6e06a33b241bae3e3d6c016bb5
srymaker0 <zyjay0@gmail.com>
2021-11-02 20:03:51 +0800

    feat : 增加心跳包结构体和work_on_reactor函数

--- common/pihealth.h
+++ common/pihealth.h
@@ -7,10 +7,21 @@
 
 #ifndef _PIHEALTH_H
 #define _PIHEALTH_H
+
+#define PI_HEART 0x01
+#define PI_ACK 0x02
+#define PI_FIN 0x04
+
+struct pihealth_msg_ds{
+    int type;
+};
+
 struct client_ds {
     int sockfd;
-    int isonline;
+    int isonline;               // >0表示在线 数字为心跳次数
     struct sockaddr_in addr;
 };
+void *work_on_reactor(void *arg);
+void heart_beat(int signum);
 void *do_login(void *arg);
 #endif
